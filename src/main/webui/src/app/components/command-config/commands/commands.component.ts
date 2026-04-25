f744bf7cbd5fed695a4ec8d6a8607881c2f4b286
Niels <info@niels.dev>
2026-04-25 15:00:09 +0200

    Add/remove commands

--- src/main/webui/src/app/components/command-config/commands/commands.component.ts
+++ src/main/webui/src/app/components/command-config/commands/commands.component.ts
@@ -1,25 +1,36 @@
 import { ChangeDetectionStrategy, Component, computed, effect, inject, input, Type, viewChildren } from '@angular/core';
 import { Command, Commands, CommandType } from '../../../models/generated/backend.types';
-import { MatButton } from '@angular/material/button';
+import { MatIconButton, MatMiniFabButton } from '@angular/material/button';
 import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 import { CommandsService } from './commands-service';
 import { commandComponentMap, validateCommands } from './commands.components';
 import { FieldTree } from '@angular/forms/signals';
-import { MatAccordion, MatExpansionPanel, MatExpansionPanelHeader, MatExpansionPanelTitle } from '@angular/material/expansion';
-import { NgComponentOutlet } from '@angular/common';
+import { MatAccordion, MatExpansionPanel, MatExpansionPanelDescription, MatExpansionPanelHeader, MatExpansionPanelTitle } from '@angular/material/expansion';
+import { NgComponentOutlet, TitleCasePipe } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+
+const grpToIdx = new Map([['standard', 0], ['obs', 1], ['wavelink', 2], ['voicemeeter', 3]]);
+
+function commandGroupIndexOf(grp: string): number {
+  return grpToIdx.get(grp) ?? 100;
+}
 
 @Component({
   selector: 'app-commands',
   imports: [
-    MatButton,
     MatMenuTrigger,
     MatMenu,
-    MatMenuItem,
     MatExpansionPanel,
     MatExpansionPanelHeader,
     MatAccordion,
     NgComponentOutlet,
-    MatExpansionPanelTitle
+    MatExpansionPanelTitle,
+    MatExpansionPanelDescription,
+    MatIconButton,
+    MatIcon,
+    MatMenuItem,
+    TitleCasePipe,
+    MatMiniFabButton
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './commands.component.html',
@@ -28,17 +39,18 @@ import { NgComponentOutlet } from '@angular/common';
 export class CommandsComponent {
   private readonly commandService = inject(CommandsService);
   commands = input.required<FieldTree<Commands>>();
-  private accordion = viewChildren(MatExpansionPanel);
-
-  private keepAccordionOpen = effect(this.initialFirstThenLastEffect());
-
   type = input.required<'dial' | 'button'>();
 
-  protected availableCommands = computed(() => (this.commandService.commands.value() ?? []).filter(c => c.kind === this.type()));
-  private validateCommands = effect(() => validateCommands(this.availableCommands()));
+  private readonly accordion = viewChildren(MatExpansionPanel);
+  private keepAccordionOpen = effect(this.initialFirstThenLastEffect());
+
+  private allCommands = computed(() => (this.commandService.commands.value() ?? []).filter(c => c.kind === this.type()));
+  protected commandGroups = computed(() => Object.keys(this.groupedCommands()).sort(commandGroupIndexOf));
+  protected groupedCommands = computed(() => Object.groupBy(this.allCommands().toSorted((l, r) => l.name.localeCompare(r.name)), c => c.category));
+  private validateCommands = effect(() => validateCommands(this.allCommands()));
 
   protected nameOf(_type: string) {
-    return this.availableCommands().find(c => c.command === _type)?.name ?? 'unknown';
+    return this.allCommands().find(c => c.command === _type)?.name ?? 'unknown';
   }
 
   protected componentFor(cmd: Command): Type<unknown> | undefined {
@@ -62,5 +74,10 @@ export class CommandsComponent {
       this.accordion().at(idx)?.open();
       first = false;
     };
+  }
+
+  protected delete(index: number, event?: MouseEvent) {
+    event?.stopPropagation();
+    this.commands().commands().value.update(val => val.filter((_, i) => i !== index));
   }
 }
